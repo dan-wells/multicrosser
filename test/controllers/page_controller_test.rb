a63ef44dcb49048835de327abc16e7d347a97243
Dan Wells <danwells396@gmail.com>
2026-06-14 15:29:12 +0100

    fix failing hompage rendering smoke test
    this breaks with the two-column series layout change on the homepage in
    test environments with empty series data: stub it to verify that the
    rest of the homepage renders without issue

--- test/controllers/page_controller_test.rb
+++ test/controllers/page_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
+require 'minitest/mock'
 
 class PageControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get root_url
-    assert_response :success
+    sample = Crossword.new(
+      'title' => 'Cryptic crossword No 12345',
+      'series' => 'cryptic',
+      'identifier' => '12345',
+      'date' => '2026-06-01T00:00:00Z',
+    )
+    Series.stub :get_all, [['cryptic', [sample]]] do
+      get root_url
+      assert_response :success
+    end
   end
-
 end
